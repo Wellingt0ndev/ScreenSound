fdf5fae179c0b1f1e98f0bb0b7d257bf22f0cf15
Wellington Ferreira <wellington_006@hotmail.com>
2023-07-28 15:02:27 -0300

    First commit

---
+++ ScreenSound/Program.cs
@@ -0,0 +1,13 @@
+﻿Episodio ep1 = new(1, "Técnicas de facilitação", 45);
+ep1.AdicionarConvidados("Maria");
+ep1.AdicionarConvidados("Pedro");
+
+
+Episodio ep2 = new(2, "Técnicas de aprendizado", 67);
+ep2.AdicionarConvidados("Fernando");
+ep2.AdicionarConvidados("Marcos");
+
+Podcast podcast = new("Podcast especial", "Daniel");
+podcast.AdicionarEpisodios(ep1);
+podcast.AdicionarEpisodios(ep2);
+podcast.ExibirDetalhes();
